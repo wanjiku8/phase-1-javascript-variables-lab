513799a9684483da18b0f75671cf8a76801ad449
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-04 18:40:51 +0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "Susan Smith";
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
